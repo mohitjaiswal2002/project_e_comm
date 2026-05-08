aea71d37477956f82de922e15f125ef462918e01
mohitjaiswal2002 <vmohitjaiswal@gmail.com>
2026-05-09 02:11:19 +0530

    removed unnecesspry code

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-from pyspark.sql import SparkSession
 import logging
-import os
-os.environ["HADOOP_HOME"] = r"C:\hadoop"
-os.environ["PATH"] = r"C:\hadoop\bin;" + os.environ.get("PATH", "")
-
+from pyspark.sql import SparkSession
 from src.read_data import read_data
 from src.transform import clean_data, transform_data
 from src.write_data import write_data
@@ -14,26 +10,16 @@ logging.basicConfig(
 )
 
 def main():
-
     spark = SparkSession.builder \
         .appName("Ecommerce Pipeline") \
         .getOrCreate()
 
-    # Read
     df = read_data(spark, "data/data.csv")
-
-    # Clean
     df_clean = clean_data(df)
-
-    # Transform
     df_final, country_df, customer_df = transform_data(df_clean)
-
-    # Write Parquet (for project)
     write_data(df_final, "output_parquet", "parquet")
 
-    # Stop Spark
     spark.stop()
 
-
 if __name__ == "__main__":
-    main() 
+    main()
--- src/write_data.py
+++ src/write_data.py
@@ -1,32 +1,17 @@
 import logging
-import os
-import shutil
 
 logger = logging.getLogger(__name__)
 
 def write_data(df, path, file_format="parquet"):
-
     logger.info(f"Writing data to {path} in {file_format}")
 
-    # delete existing folder
-    if os.path.exists(path):
-        shutil.rmtree(path)
+    writer = df.coalesce(1).write.mode("overwrite")
 
     if file_format == "csv":
-        (df.coalesce(1)
-         .write
-         .mode("overwrite")
-         .option("header", True)
-         .csv(path))
-
+        writer.option("header", True).csv(path)
     elif file_format == "parquet":
-         (df.coalesce(1)
-        .write
-        .mode("overwrite")
-        .option("header", True)
-        .csv(path))
-
+        writer.parquet(path)
     else:
-        raise ValueError("Unsupported format")
+        raise ValueError(f"Unsupported format: {file_format}")
 
     logger.info("Write completed")
